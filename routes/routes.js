94472f4ab0c1146d7cbc5c850d53cc473fef4cb5
Jonathan Carson <carsonjd@colorado.edu>
2017-12-07 14:12:15 -0700

    routes initiation

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+if (process.env.NODE_ENV !== 'test') app.use(morgan('dev'))
+
+app.disable('x-powered-by')
+app.use(bodyParser.json())
+app.use(cors())
