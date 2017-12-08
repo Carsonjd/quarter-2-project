30f4a6d31c44ef2a9f8c0281a6b2e48371854acc
Jonathan Carson <carsonjd@colorado.edu>
2017-12-08 13:19:34 -0700

    l

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-$('document').ready({
+
+$('document').ready(() => {
   console.log('bananas');
   const darkSkyKey = '1163de32b0c568e75278023a3768f8a3';
-  //const baseURL = 'http://localhost:3000'
+
+  $('login').on('click', () => {
+
+  })
 
   let lat = 40.2549
   let long = 105.6160
   axios.get(`https://api.darksky.net/forecast/${darkSkyKey}/${lat},${long}`)
-    .then((res) => console.log(res);)
+    .then((res) => console.log(res))
 
 })
