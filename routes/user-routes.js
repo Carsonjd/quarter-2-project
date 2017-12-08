c8128cf2fce32b4a9eb55ed8a0fd8301fa181c70
Patrick Glynn <pjglynn57@gmail.com>
2017-12-08 15:41:42 -0700

    messing w routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,7 +4,9 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
+const morgan = require('morgan');
+const knex = require('../knex');
+const router = express.Router();
 
 if (process.env.NODE_ENV !== 'test') app.use(morgan('dev'))
 
@@ -12,9 +14,14 @@ app.disable('x-powered-by')
 app.use(bodyParser.json())
 app.use(cors())
 
+console.log(knex('users'));
 
 router.get('/', (req,res,next) => {
   knex('users')
   .where('id', req.body.id)
   .then(([result])=>result)
 });
+
+router.post('/users', (req, res, next) => {
+  console.log(req);
+})
