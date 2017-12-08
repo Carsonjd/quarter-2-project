87df6450d4a43175c22398f53a3bc96571ec2d22
Judah Martin Trimmer <jutr4020@colorado.edu>
2017-12-08 09:15:36 -0700

    knex files started

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://...'
+},
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://...'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  }
+};
