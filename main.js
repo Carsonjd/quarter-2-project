766cffb039640844bb4bcf16db5f86bec6cd7413
Jonathan Carson <carsonjd@colorado.edu>
2017-12-08 13:46:10 -0700

    added d3 script

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 $('document').ready(() => {
   console.log('bananas');
   const darkSkyKey = '1163de32b0c568e75278023a3768f8a3';
-  // const baseURL = 'http://localhost:3000'
 
   let lat = 40.2549
-  let long = 105.6160
+  let long = -105.6160
   axios.get(`https://dark-star-proxy.herokuapp.com/forecast/${darkSkyKey}/${lat},${long}`)
     .then((res) => console.log(res))
 
