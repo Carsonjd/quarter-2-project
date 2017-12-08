b0d50f859eb1fa257d41698467ddcb0e8e405753
Jonathan Carson <carsonjd@colorado.edu>
2017-12-08 11:55:24 -0700

    getting orged

--- routes/locations-routes.js
+++ routes/locations-routes.js
@@ -1,1 +1,23 @@
-:)
+$('document').ready({
+  console.log('bananas');
+  const baseURL = 'http://localhost:3000'
+
+  $('#compare').on('click', (event) => {
+    const user1 = $('#username-1').val()
+    const user2 = $('#username-2').val()
+    if (user1 && user2) {
+      axios.all([
+        axios.get(`https://api.github.com/users/${user1}`),
+        axios.get(`https://api.github.com/users/${user2}`)
+      ]).then((res) => {
+        showDataOne(res[0].data)
+        showDataTwo(res[1].data)
+      })
+    } else {
+      alert('Invalid input')
+    }
+  })
+
+  
+
+})
