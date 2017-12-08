5f54627130c4bf373aa681eff67f6a26e206ed92
Judah Martin Trimmer <jutr4020@colorado.edu>
2017-12-08 12:03:49 -0700

    user schema done, not seeded

--- migrations/20171208102609_users.js
+++ migrations/20171208102609_users.js
@@ -1,10 +1,16 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', table => {
-    
+    table.increments()
+    table.string('First Name').notNullable().defaultsTo('')
+    table.string('Last Name').notNullable().defaultsTo('')
+    table.string('User Name').notNullable().defaultsTo('')
+    table.string('Email').notNullable().defaultsTo('')
+    table.string('Password').notNullable().defaultsTo('')
+    table.timestamps(true, true)
   })
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable('users')
 };
