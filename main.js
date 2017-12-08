06a32eb2b982f1b004b7c418194c21ec579dc35e
Patrick Glynn <pjglynn57@gmail.com>
2017-12-08 13:16:26 -0700

    added heroku proxy server

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-$('document').ready({
-  console.log('bananas');
+$('document').ready(() => {
+  // console.log('bananas');
   const darkSkyKey = '1163de32b0c568e75278023a3768f8a3';
-  //const baseURL = 'http://localhost:3000'
+  // const baseURL = 'http://localhost:3000'
 
   let lat = 40.2549
   let long = 105.6160
-  axios.get(`https://api.darksky.net/forecast/${darkSkyKey}/${lat},${long}`)
-    .then((res) => console.log(res);)
+  axios.get(`https://dark-star-proxy.herokuapp.com/forecast/${darkSkyKey}/${lat},${long}`)
+    .then((res) => console.log(res))
 
 })
