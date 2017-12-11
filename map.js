e46bc6fe4a0a5e44ebf0d6f102c89bccff734e32
Stephanie Marvez <marvez@gmail.com>
2017-12-11 13:35:56 -0700

    map on page

--- map.js
+++ map.js
@@ -5,16 +5,22 @@ $('document').ready(function () {
 mapboxgl.accessToken =        'pk.eyJ1IjoiY2xvdWR2dSIsImEiOiJjamIyZ3hzeWUxaGtlMnduMnF3Mm56eTI0In0.f-dt5_iZmOhTgDB9MOrU0Q';
 
 var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/cloudvu/cjb2izk4r5btc2so0mhhejjqa',
-  center: [-105.2838747, 40.0165447],
-  pitch: 60,
-  zoom: 10,
-  bearing: -17.6,
-});
-map.addControl(new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true,
-      },
-    trackUserLocation: false,
-  }));
+    container: 'map',
+    style: 'mapbox://styles/cloudvu/cjb2izk4r5btc2so0mhhejjqa',
+  });
+
+// var map = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/cloudvu/cjb2izk4r5btc2so0mhhejjqa',
+//   center: [-105.2838747, 40.0165447],
+//   pitch: 60,
+//   zoom: 10,
+//   bearing: -17.6,
+// });
+
+// map.addControl(new mapboxgl.GeolocateControl({
+//     positionOptions: {
+//         enableHighAccuracy: true,
+//       },
+//     trackUserLocation: false,
+//   }));
