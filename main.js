54389682f2f9f6a2138872bb146003cc390c470e
Jonathan Carson <carsonjd@colorado.edu>
2017-12-11 16:36:20 -0700

    lots more d3 working

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $('document').ready(() => {
   console.log('bananas');
 
   const darkSkyKey = '1163de32b0c568e75278023a3768f8a3';
-  var data = []
+  var dataArr = []
   let lat = 40.2549
   let long = -105.6160
   axios.get(`https://dark-star-proxy.herokuapp.com/forecast/${darkSkyKey}/${lat},${long}`)
@@ -18,27 +18,42 @@ $('document').ready(() => {
           humidity: el.humidity,
           windSpeed: el.windSpeed
         }
-        data.push(hour)
+        dataArr.push(hour)
         i++
       })
       var formatTime = d3.timeFormat("%m/%d/%y %H:%m:%S %p");
+      var parseTime = d3.timeParse("%m/%d/%y %H:%m:%S %p");
+      var datearray = []
+      colorrange = ["#045A8D", "#2B8CBE", "#74A9CF", "#A6BDDB", "#D0D1E6", "#F1EEF6"];
+      //colorrange = ["#980043", "#DD1C77", "#DF65B0", "#C994C7", "#D4B9DA", "#F1EEF6"];
+      //colorrange = ["#B30000", "#E34A33", "#FC8D59", "#FDBB84", "#FDD49E", "#FEF0D9"];
+      strokecolor = colorrange[0];
 
       var stack = d3.stack()
         .keys(["appTemp", "cloudCover", "dewPoint", "humidity", "windSpeed"])
         .order(d3.stackOrderNone)
         .offset(d3.stackOffsetWiggle);
-      var series = stack(data);
+      var series = stack(dataArr);
 
-      var margin = {top: 20, right: 20, bottom: 50, left: 70},
-        width = 1000 - margin.left - margin.right,
-        height = 800 - margin.top - margin.bottom;
+      var margin = {top: 20, right: 20, bottom: 50, left: 70}
+      var width = document.body.clientWidth - margin.left - margin.right;
+      var height = 900 - margin.top - margin.bottom;
 
       var svg = d3.select("svg")
         .attr("width", width)
         .attr("height", height);
 
+      var tooltip = d3.select("body")
+        .append("div")
+        .attr("class", "remove")
+        .style("position", "absolute")
+        .style("z-index", "20")
+        .style("visibility", "hidden")
+        .style("top", "30px")
+        .style("left", "55px");
+
       var x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) {
+        .domain(d3.extent(dataArr, function(d) {
           return d.time;
         }))
         .range([0, width]);
@@ -59,7 +74,7 @@ $('document').ready(() => {
         .range([height, 0]);
 
       var color = d3.scaleLinear()
-        .range(["#51D0D7", "#31B5BB"]);
+        .range(colorrange);
 
       var area = d3.area()
         .x(function(d) {
@@ -76,36 +91,47 @@ $('document').ready(() => {
       svg.selectAll("path")
         .data(series)
         .enter().append("path")
+        .attr("class", "layer")
         .attr("d", area)
-        .style("fill", function() {
-          return color(Math.random());
-        });
+        .attr("transform", "translate(0, -225)")
+        .style("fill", function(d, i) { return color(i); });
 
       svg.append("g")
       .attr("class", "axis")
-      .attr("transform", "translate(0," + height + ")")
+      .style("fill", "black")
+      .attr("transform", "translate(0,600)")
       .call(d3.axisBottom(x)
               .tickFormat(d3.timeFormat("%m/%d/%y %H:%m:%S %p")))
       .selectAll("text")
         .style("text-anchor", "end")
-        .style("color", "black")
         .attr("dx", "-.8em")
         .attr("dy", ".15em")
         .attr("transform", "rotate(-65)");
 
         // text label for the x axis
       svg.append("text")
+          .style("fill", "black")
           .attr("transform",
                 "translate(" + (width/2) + " ," +
                                (height + margin.top + 20) + ")")
           .style("text-anchor", "middle")
           .text("Date");
 
-      // svg.append("g")
-      //   .attr("class", "axis axis--x")
-      //   .attr("transform", "translate(0," + (height) + ")")
-      //   .call(xAxis);
+          // Add the y Axis
+      svg.append("g")
+          .attr("class", "axis")
+          .style("fill", "black")
+          .attr("transform", "translate(30, 0)")
+          .call(d3.axisLeft(y));
 
+      // text label for the y axis
+      svg.append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 0 - margin.left)
+          .attr("x",0 - (height / 2))
+          .attr("dy", "1em")
+          .style("text-anchor", "middle")
+          .text("Value");
 
             function transition() {
               var t;
@@ -115,6 +141,66 @@ $('document').ready(() => {
                 .duration(2500)
                 .attr("d", area);
             }
+      ////////////////////////////////////////////////////////////
+    svg.selectAll(".layer")
+    .attr("opacity", 1)
+    .on("mouseover", function(d, i) {
+      svg.selectAll(".layer").transition()
+      .duration(250)
+      .attr("opacity", function(d, j) {
+        return j != i ? 0.6 : 1;
+    })})
+
+    .on("mousemove", function(d, i) {
+      mousex = d3.mouse(this);
+      mousex = mousex[0];
+      var invertedx = x.invert(mousex);
+      var invDate = `${invertedx.getDate()} ${invertedx.getHours()}`
+      var selected = d.key;
+      var pro = dataArr.filter((el) => {
+        var elDate = `${el.time.getDate()} ${el.time.getHours()}`
+        return elDate === invDate
+      })[0][selected]
+      console.log(pro);
+
+      d3.select(this)
+      .classed("hover", true)
+      .attr("stroke", strokecolor)
+      .attr("stroke-width", "0.5px"),
+      tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "visible");
+
+    })
+    .on("mouseout", function(d, i) {
+     svg.selectAll(".layer")
+      .transition()
+      .duration(250)
+      .attr("opacity", "1");
+      d3.select(this)
+      .classed("hover", false)
+      .attr("stroke-width", "0px"), tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "hidden");
+  })
+
+  var vertical = d3.select(".chart")
+        .append("div")
+        .attr("class", "remove")
+        .style("position", "absolute")
+        .style("z-index", "19")
+        .style("width", "1px")
+        .style("height", "380px")
+        .style("top", "10px")
+        .style("bottom", "30px")
+        .style("left", "0px")
+        .style("background", "#fff");
+
+  d3.select(".chart")
+      .on("mousemove", function(){
+         mousex = d3.mouse(this);
+         mousex = mousex[0] + 5;
+         vertical.style("left", mousex + "px" )})
+      .on("mouseover", function(){
+         mousex = d3.mouse(this);
+         mousex = mousex[0] + 5;
+         vertical.style("left", mousex + "px")});
 
 
     })
