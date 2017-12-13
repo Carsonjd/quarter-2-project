d25d7b524896fc73fb3e5583fd745e178dc1c662
Jonathan Carson <carsonjd@colorado.edu>
2017-12-12 19:18:54 -0700

    fixing a lot

--- public/map.js
+++ public/map.js
@@ -2,14 +2,13 @@ var userLocations;
 
 $('document').ready(function() {
   console.log('bananas');
+
   $.get('/user-favs', (result) => {
     console.log('your favorite locations are right here!', result.locations);
     userLocations = result.locations;
   }).then((result) => {
     createMarkers(result.locations)
   })
-  // console.log(userLocations);
-
 
 });
 
@@ -52,173 +51,10 @@ map.on('load', function() {
     }
   });
 
-  function removePopUps(){
+  function removePopUps() {
     var popUps = document.getElementsByClassName('mapboxgl-popup');
     if (popUps[0]) popUps[0].remove();
   };
-
-  //started here
-
-  var menuOpen = false;
-  function menuView (){
-    menuOpen = true;
-    $('#menu-main').text('hide');
-    $('.red-glow-circle').animate({
-      'opacity': '0.9'
-    },300);
-    $('body').css('background-blend-mode', 'color-dodge');
-    mapDim();
-    $('h1').animate({
-      'margin-top': '9%'
-    }, 300);
-    $('.drop-main').animate({
-      'top': '5%'
-    },300);
-    $('.about').css('opacity', '0.1');
-    $('.locations').css('opacity', '0.1');
-    $('.form-container').animate({
-      'opacity': '0.1'
-    },300);
-  };
-
-  function menuHide (){
-    menuOpen = false;
-    mapBright();
-    $('#menu-main').text('menu');
-    $('.red-glow-circle').animate({
-      'opacity': '0.35'
-    },300);
-    $('body').css('background-blend-mode', 'normal');
-    $('.drop-main').animate({
-      'top': '-75%'
-    },300);
-    $('h1').animate({
-      'margin-top': '300%'
-    }, 300);
-    $('.about').css('opacity', '0.6');
-    $('.locations').css('opacity', '0.9');
-    $('.form-container').animate({
-      'opacity': '0.9'
-    },300);
-  };
-
-  function homeShow (){
-    $('body').css('background-image','url(DSC00858.jpg)');
-    $('.about').animate({
-      'opacity': '0.6'
-    },300).fadeIn(300);
-  };
-
-  function homeHide (){
-    $('body').css('background-image','url()');
-    $('.about').fadeOut(300);
-  };
-
-  function mapShow (){
-    $('#map').css('display', 'block');
-    $('body').css('background-image', 'url()');
-    $('#map').animate({
-      'opacity':'1.0'
-    }, 300);
-  };
-
-  function mapHide (){
-    $('#map').css('display', 'none');
-  };
-
-  function mapDim (){
-    $('#map').animate({
-      'opacity':'0.3'
-    }, 300);
-  };
-
-  function mapBright (){
-    $('#map').animate({
-      'opacity':'1.0'
-    }, 300);
-  };
-
-  function formShow (){
-    $('.form-container').animate({
-      'top': '30%'
-    },300);
-  };
-
-  function formHide (){
-    $('.form-container').animate({
-      'top': '100%'
-    },300);
-  };
-
-  function locationListShow (){
-    $('.locations').animate({
-      'top': '10%'
-    },300).fadeIn(100);
-  };
-
-  function locationListHide (){
-    $('.locations').animate({
-      'top':'100%'
-    },300).fadeOut(500);
-  };
-
-  //Menu click/tap events
-  $('#menu-main').click(function(event){
-    if(menuOpen === false){
-      menuView();
-    } else {
-      menuHide();
-    }
-  });
-
-  $('#menu-home').click(function(event){
-    homeShow();
-    mapHide();
-    menuHide();
-    locationListHide();
-    formHide();
-  });
-
-  $('#menu-map').click(function(event){
-    menuHide();
-    mapShow();
-    homeHide();
-    locationListHide();
-    formHide();
-  });
-
-  $('#menu-locations').click(function(event){
-    removePopUps();
-    mapDim();
-    menuHide();
-    homeHide();
-    formHide();
-    locationListShow();
-  });
-
-  $('#locations').click(function(event){
-    locationListHide();
-    mapShow();
-  });
-
-  $('#menu-add').click(function(event){
-    removePopUps();
-    homeHide();
-    formShow();
-    menuHide();
-    locationListHide();
-    mapDim();
-  });
-
-//ended here
-
-
-  map.on('mousemove', function (e) {
-    document.getElementById('info').innerHTML =
-        // e.lngLat is the longitude, latitude geographical position of the event
-        JSON.stringify(e.lngLat);
-        // console.log(document.getElementById('info').innerHTML);
-  });
 
   map.on('click', function(e) {
     removePopUps();
@@ -227,105 +63,74 @@ map.on('load', function() {
     currentLoc.lat = loc.lat;
     console.log(currentLoc);
     console.log(currentLoc.lng, currentLoc.lat);
-    var popup = new mapboxgl.Popup({closeOnClick: false})
+    var popup = new mapboxgl.Popup({
+        closeOnClick: false
+      })
       .setLngLat(currentLoc)
       .setHTML('<button class="trigger" id="trigger-a">Check Forecast</button><button class="trigger" id="trigger-b">Add to Favorites</button><button class="trigger" id="trigger-c" hidden>Submit</button><input type="text" placeholder="Location Name" id="desc" hidden></input>')
       .addTo(map);
   });
 
-  $('#map').on('click', '.trigger-a', function(ev){
+  $('#map').on('click', '#trigger-a', function(ev) {
     console.log('you have clicked on forecast');
     window.location = `./data.html?lat=${currentLoc.lat}&long=${currentLoc.lng}`
   })
 
-  $('#map').on('click', '#trigger-b', function(ev){
-    console.log(currentLoc);
+  $('#map').on('click', '#trigger-b', function(ev) {
     $('#desc').attr('hidden', false);
     $('#trigger-a').remove();
     $('#trigger-b').remove();
     $('#trigger-c').css('display', 'block');
-    $.post('/add-location', currentLoc, (success) => {
-      console.log(success);
-    })
-    console.log('you have clicked on add location');
   })
 
-  $('#map').on('click', '#trigger-c', function(ev){
+  $('#map').on('click', '#trigger-c', function(ev) {
     currentLoc.location_name = $('#desc').val();
-    // console.log(locationName);
     $.post('/add-location', currentLoc, (success) => {
       console.log(success);
     })
-    .setLngLat(currentLoc)
-    .setHTML('<button class="trigger" id="trigger-a">Check Forecast</button><button class="trigger" id="trigger-b">Add to Favorites</button><button class="trigger" id="trigger-c" hidden>Submit</button><input type="text" placeholder="Location Name" id="desc" hidden></input>')
-    .addTo(map);
-});
-
-$('#map').on('click', '.trigger-a', function(ev) {
-  console.log('you have clicked on forecast');
-
-  window.location = `./data.html?lat=${currentLoc.lat}&long=${currentLoc.lng}`
-})
-
-$('#map').on('click', '#trigger-b', function(ev) {
-  $('#desc').attr('hidden', false);
-  $('#trigger-a').remove();
-  $('#trigger-b').remove();
-  $('#trigger-c').css('display', 'block');
-})
-
-$('#map').on('click', '#trigger-c', function(ev) {
-  currentLoc.location_name = $('#desc').val();
-  // console.log(locationName);
-  $.post('/add-location', currentLoc, (success) => {
-    console.log(success);
+    removePopUps();
+    window.location = `./data.html?lat=${currentLoc.lat}&long=${currentLoc.lng}`
   })
-  removePopUps();
-  window.location = `./data.html?lat=${currentLoc.lat}&long=${currentLoc.lng}`
+
+  // MapboxGeocoder
+
+  map.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken
+  }));
+
+  // var newMark = new mapboxgl.Marker()
+  //   .setLngLat([
+  //     -105.28387478,
+  //     40.0165447
+  //   ])
+  //   .addTo(map);
+
+  // $('.marker').on('click')
+
+  function createMarkers(array) {
+    array.forEach(function(marker) {
+      var element = document.createElement('div');
+      element.classList.add('marker');
+      var latlng = [
+        parseFloat(parseFloat(marker.latitude).toFixed(7)),
+        parseFloat(parseFloat(marker.longitude).toFixed(7))
+      ]
+      new mapboxgl.Marker(element)
+        .setLngLat(latlng)
+        .addTo(map)
+
+      // element.addEventListener('click', function(event){
+      //   console.log('you clicked it!');
+      // //   var activeItem = $('active');
+      // //   // flyToLocation(marker);
+      // //   // createPopUp(marker);
+      // //   event.stopPropagation();
+      // //   // if (activeItem[0]) {
+      // //   //   activeItem[0].classList.remove('active');
+      // //   // }
+      // //   var location = document.getElementById('location-' +   userLocations.indexOf(marker));
+      // //   location.classList.add('active');
+      // });
+    });
+  }
 })
-
-// MapboxGeocoder
-
-map.addControl(new MapboxGeocoder({
-  accessToken: mapboxgl.accessToken
-}));
-
-// var newMark = new mapboxgl.Marker()
-//   .setLngLat([
-//     -105.28387478,
-//     40.0165447
-//   ])
-//   .addTo(map);
-
-// $('.marker').on('click')
-
-function createMarkers(array) {
-  array.forEach(function(marker) {
-    var element = document.createElement('div');
-    element.classList.add('marker');
-    var latlng = [
-      parseFloat(parseFloat(marker.latitude).toFixed(7)),
-      parseFloat(parseFloat(marker.longitude).toFixed(7))
-    ]
-    console.log(latlng);
-    new mapboxgl.Marker(element)
-      .setLngLat(latlng)
-      .addTo(map)
-
-    // element.addEventListener('click', function(event){
-    //   console.log('you clicked it!');
-    // //   var activeItem = $('active');
-    // //   // flyToLocation(marker);
-    // //   // createPopUp(marker);
-    // //   event.stopPropagation();
-    // //   // if (activeItem[0]) {
-    // //   //   activeItem[0].classList.remove('active');
-    // //   // }
-    // //   var location = document.getElementById('location-' +   userLocations.indexOf(marker));
-    // //   location.classList.add('active');
-    // });
-  })
-}
-
-
-// module.exports = {currentLoc}
