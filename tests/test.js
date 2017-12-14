ec02e6745720efcdf4c03b5c8035c2da52832044
Stephanie Marvez <marvez@gmail.com>
2017-12-14 15:24:59 -0700

    test structure set up

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,66 @@
-const assert = require('assert')
-const expect = require('chai').expect
+const expect = chai.expect;
+const should = chai.should();
+var assert = chai.assert;
 
-
-
-
-describe('..', function() {
-  it('...', function() {
-    assert.equal(..., ...());
+describe('menuView', function(){
+  it('is a function', function(){
+    expect(menuView).to.be.a('function')
+  });
 });
+
+var Test = {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [1231223,1233123]
+    },
+    "properties": {
+      "address": "23iuf12308ryfef",
+      "city": "fnodvnwofh034",
+      "country": "donvowef83023f",
+    }
+  };
+
+describe('featureBlankReset', function(){
+  it('resets the object being used to create new locations', function(){
+    expect(featureBlankReset(Test)).to.deep.equal({
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": []
+        },
+        "properties": {
+          "address": "",
+          "city": "",
+          "country": "",
+        }
+      });
+  });
+});
+
+describe('mapboxgl.Map', function(){
+  it('is a constructor function', function(){
+    assert(new mapboxgl.Map()).not.to.Throw('Error')
+  });
+});
+
+describe('MapboxGeocoder', function(){
+  it('is a constructor function', function(){
+    assert(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken
+    })).to.not.Throw('Error')
+  });
+});
+
+// describe('getCurrentPosition', function(){
+//   it('returns a geoJSON object', function(){
+//     expect(typeof navigator.geolocation.getCurrentPosition(function(position){})).to.equal('object')
+//   });
+// });
+
+
+describe('removePopUps', function(){
+  it('is not returning an error', function(){
+    expect(removePopUps).to.not.Throw('Error')
+  });
 });
