2148a7843ca28cb2a459c21ca02dc65d44ffb78b
Jonathan Carson <carsonjd@colorado.edu>
2017-12-15 09:30:57 -0700

    y axis

--- public/main.js
+++ public/main.js
@@ -345,13 +345,13 @@ $('document').ready(() => {
         .call(d3.axisLeft(y));
 
       // text label for the y axis
-      svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x", 0 - (height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .text("Value");
+      // svg.append("text")
+      //   .attr("transform", "rotate(-90)")
+      //   .attr("y", 0 - margin.left)
+      //   .attr("x", 0 - (height / 2))
+      //   .attr("dy", "1em")
+      //   .style("text-anchor", "middle")
+      //   .text("Value");
 
       function transition() {
         var t;
